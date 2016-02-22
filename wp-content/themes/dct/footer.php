b905a79349d5618dc23876df7019191f9f8ccb7f
Radian <craziestdavid@gmail.com>
2016-02-22 18:52:16 +0800

    Fix phone number in footer

--- wp-content/themes/dct/footer.php
+++ wp-content/themes/dct/footer.php
@@ -4,13 +4,14 @@
                         <div class="container">
                             <div class="row">
                                 <div class="col-sm-8 ">
-                                    <ul class="none" style="margin-top:20px;">
+                                    <ul class="none" style="margin-top:20px; line-height:130%;">
                                         <li>DCT</li>
-                                        <li>Tel:886-2-29393091 Fax: 886-2-29379611</li>
-                                        <li>Address: NO.64,Sec.2,ZhiNan Rd.,Wenshan District,Taipei City 11605,Taiwan (R.O.C)</li>
+                                        <li>Tel: <a href="tel:+886229393091p62670">886-2-29393091#62670</a> Fax: <a href="tel:+886229378629">886-2-29378629</a></li>
+                                        <li>Address: <a href="https://goo.gl/qT9zn3
+" target="_blank" >NO.64,Sec.2,ZhiNan Rd.,Wenshan District,Taipei City 11605,Taiwan (R.O.C)</a></li>
                                         <li>Emergency Information Line:  0919099119 / 66119、66110 (on campus)</li>
                                     </ul>
-                                    
+
                                 </div>
                                 <div class="col-sm-8">
                                 </div>
@@ -21,9 +22,9 @@
                         </div>
                     </div>
                     <!-- /footer -->
-                    
+
                 </div>
-                
+
              <?php wp_footer() ?>
             </body>
         </html>
