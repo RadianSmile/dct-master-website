91cfc197adff162f321801507ac296f8705856ff
Radian <craziestdavid@gmail.com>
2016-02-23 07:21:29 +0800

    Added student

--- wp-content/themes/dct/index.php
+++ wp-content/themes/dct/index.php
@@ -160,11 +160,75 @@
 		</div>
 	</div>
 	<hr>
+<?php
+	// $ary[ $term_num ][ user_id ]
+	$display_array;
+
+
+
+	// get all students
+	$studnet_args = array(
+		'role'		=>	'student',
+		'meta_key'	=>	'student_id',
+		'orderby'	=>	'meta_value',
+		'order'		=>	'asc'
+	);
+
+	$students = get_users( $studnet_args );
+
+	// put users in display array, in order to set the term_num
+	foreach( $students as $index => $data ):
+		$term_num = get_user_meta( $data->ID, 'term_num', true );
+
+		if( empty( $display_array[ $term_num ] ) )
+			$display_array[ $term_num ] = array();
+		array_push( $display_array[ $term_num ], $data );
+	endforeach;
+
+	// sort array DESC, make newer student appear first
+	krsort($display_array);
+?>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 ">
+
+        <div class="divTitle">
+          <?=$term?>th <span></span>
+        </div>
+<?php foreach( $display_array as $term => $user_array ): ?>
+<?php foreach( $user_array as $index => $data ):
+
+
+    $attachment_id = get_user_meta( $data->ID, 'person_photo', true );
+    $photo = wp_get_attachment_image_src( $attachment_id, 'thumbnail', true );
+    $photoUrl = esc_attr( $photo[0] );
+
+    if( $attachment_id == null )
+    	$photoUrl = get_template_directory_uri() . '/img/WhosWho.png';
+
+?>
+      <div class="col-md-3 listuser text-center">
+      	<a href="<?= site_url().'/author/'.$data->user_login ?>">
+      		<img src="<?= $photoUrl ?>" alt="<?= $data->display_name ?>" class="img-circle" />
+      		<h4> <?= $data->display_name ?> </h4>
+      		<p> <?= get_user_meta( $data->ID, 'person_title', true ) ?> </p>
+      	</a>
+      </div>
+<?php endforeach; ?>
+
+
+      </div>
+    </div>
+  </div>
+
 </div>
 <script>
 	$('body').attr('id', 'index');
 	$('li.index').attr('class', 'active index');
 </script>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 
 
 <?php get_footer() ?>
